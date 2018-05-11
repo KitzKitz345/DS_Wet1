6d21b6d98cb905f6b283fd732bfed61f024f2811
Amit Tsvieli <amit.tsvi@gmail.com>
2018-05-11 19:16:28 +0300

    remove function added

--- Tree.h
+++ Tree.h
@@ -118,22 +118,109 @@ class Tree {
         Tree* T = this;
         while(T->father != nullptr){
             Tree* current_father = T->father;
-            //calculate heights before insertion
+            //calculate heights before insertion??
+            /*
             int father_height = max(current_father->h_right, current_father->h_left) + 1;
             int son_height = max(T->h_right, T->h_left) + 1;
             if (father_height >= son_height+1){
                 current_father->update_heights();
-                return;
+               return;
             }
+            */
             current_father->update_heights();
             int BF = current_father->h_left - current_father->h_right;
             if(BF >= 2 || BF <= -2){
                 current_father->find_roll();
-                return;
-            } else {
-                T = current_father;
             }
+            T = current_father;
         }
+    }
+
+    void remove_leaf(){
+        Tree* parent = this->father;
+        bool is_left_son = parent->lson->key == key;
+        if (is_left_son){
+            //delete parent->lson;
+            parent->lson = nullptr;
+            parent->h_left  = 0;
+        } else {
+            //delete parent->rson;
+            parent->rson = nullptr;
+            parent->h_right = 0;
+        }
+        while (parent != nullptr){
+            parent->size--;
+            parent = parent->father;
+        }
+    }
+
+    void remove_only_one_son(){
+        Tree* parent = this->father;
+        bool is_left_son = parent->lson->key == key;
+        Tree* only_son = nullptr;
+        if (this->lson == nullptr){
+            only_son = this->rson;
+        } else {
+            only_son = this->lson;
+        }
+        if (is_left_son){
+            //delete parent->lson;
+            parent->lson = only_son;
+            parent->h_left--;
+        } else {
+            //delete parent->rson;
+            parent->rson = only_son;
+            parent->h_right--;
+        }
+        while (parent != nullptr){
+            parent->size--;
+            parent = parent->father;
+        }
+    }
+
+    void switch_nodes(Tree& to_switch){
+        Tree* temp_tree = this->father;
+        if (temp_tree->lson->key == this->key) {
+            temp_tree->lson = &to_switch;
+        }else{
+            temp_tree->rson = &to_switch;
+        }
+        if (to_switch.father->lson->key == to_switch.key){
+            to_switch.father->lson = this;
+        } else {
+            to_switch.father->rson = this;
+        }
+        this->father = to_switch.father;
+        to_switch.father = temp_tree;
+
+        temp_tree = this->lson;
+        this->lson = to_switch.lson;
+        to_switch.lson = temp_tree;
+
+        temp_tree = this->rson;
+        this->rson = to_switch.rson;
+        to_switch.rson = temp_tree;
+
+        int temp_int = this->h_left;
+        this->h_left = to_switch.h_left;
+        to_switch.h_left = temp_int;
+
+        temp_int = this->h_right;
+        this->h_right = to_switch.h_right;
+        to_switch.h_right = temp_int;
+
+        temp_int = this->size;
+        this->size = to_switch.size;
+        to_switch.size = temp_int;
+    }
+
+    void remove_two_sons(Key& key){
+        Tree* next_node = this->rson;
+        while (next_node->lson != nullptr) {
+            next_node = next_node->lson;
+        }
+        this->switch_nodes(*next_node);
+        remove(this->key);
     }
 
 public:
@@ -153,6 +240,7 @@ public:
     Tree(const Tree& tree) = delete;
 
     class AlreadyExist : public std::exception {};
+    class DoesNotExist : public std::exception {};
     class EmptyTree : public std::exception {};
 
     Tree& find(Key& key) {
@@ -204,11 +292,30 @@ public:
             T->rson->father = T;
             T = T->rson;
         }
-        T->balance();
-        while(T != nullptr){
-            T->size++;
-            T = T->father;
+        Tree* T_copy = T;
+        while (T_copy != nullptr){
+            T_copy->size++;
+            T_copy = T_copy->father;
         }
+        T->balance();
+    }
+
+    void remove(Key& key){
+        Tree* T = &this->find(key);
+        if (T->key != key){
+            throw DoesNotExist();
+        }
+        Tree* parent = T->father;
+        bool is_left_son = parent->lson->key == key;
+        if (T->size == 1) {
+            T->remove_leaf();
+        } else if (T->lson == nullptr || T->rson == nullptr){ //T has only one son
+            T->remove_only_one_son();
+        } else {
+            T->remove_two_sons(key);
+        }
+        parent->balance();
+        delete T;
     }
 };
 
