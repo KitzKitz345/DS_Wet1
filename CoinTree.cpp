c4a642e477ff8becc6a2041ac5845b26ae25f7c9
Amit Tsvieli <amit.tsvi@gmail.com>
2018-05-12 13:27:19 +0300

    CoinTree getScoreBoard beginning

--- CoinTree.cpp
+++ CoinTree.cpp
@@ -2,4 +2,13 @@
 // Created by Nitzan on 06/05/2018.
 //
 
+#include <cstdlib>
 #include "CoinTree.h"
+
+
+void CoinTree::getScoreBoard(Tree<Player, Pair>* coin_tree, int **players, int *numOfPlayers){
+    int n = coin_tree->getSize();
+    Player** player_arr = new Player*[n];
+    coin_tree->inorder(player_arr);
+
+}
