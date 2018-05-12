3424394dbfeb3a1a4a1dd9215b0ea39c3530243f
n.l <nitzan.la@campus.technionn.ac.il>
2018-05-12 11:53:11 +0300

    added addPlayer and completeChalenge funcs added library1.cpp and deleted oasis.cpp Added clan field to playerTree

--- PlayerTree.h
+++ PlayerTree.h
@@ -6,14 +6,17 @@
 #define CODE_PLAYERTREE_H
 
 #include "Tree.h"
+#include "ClanTree.h"
 
 class Player {
     int player_id;
     int coins;
     int challenges;
+    Clan* clan;
+
 public:
-    explicit Player (int id, int coins, int challenges) : player_id(id), coins(coins), challenges
-            (challenges){}
+    explicit Player (int id, int coins, int challenges, Clan* clan) : player_id(id), coins(coins),
+             challenges (challenges), clan(nullptr){}
 
     ~Player();
 
@@ -28,6 +31,9 @@ public:
     int getChallenges() {
         return this->challenges;
     }
+    Clan* getClan() {
+        return this->clan;
+    }
     void addCoins(int coins) {
         this->coins += coins;
     }
@@ -38,7 +44,7 @@ public:
 
 //should new_player tree needs *?
 void addPlayer (Tree<Player, int>* player_tree, int player_id, int coins) {
-    Player* new_player = new Player(player_id,coins,0);
+    Player* new_player = new Player(player_id,coins,0,nullptr);
     player_tree->insert(player_id,*new_player);
 }
 
